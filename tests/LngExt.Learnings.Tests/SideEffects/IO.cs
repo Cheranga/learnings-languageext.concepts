49486830842bd4bc406e64508cd67122f2d4bc74
Cheranga Hatangala <chatangala@bunnings.com.au>
2022-08-24 22:02:28 +1000

    chore: Started on side effects [ci]

---
+++ tests/LngExt.Learnings.Tests/SideEffects/IO.cs
@@ -0,0 +1,36 @@
+﻿namespace LngExt.Learnings.Tests.SideEffects;
+
+public delegate Either<Error, A> IO<A>();
+public static class IOOperations
+{
+    public static IO<string> ReadAllText(string filePath) =>
+        () =>
+            Try(() => File.ReadAllText(filePath))
+                .ToEither(exception => Error.New(500, "error when reading file", exception));
+
+    public static IO<Unit> WriteAllText(string filePath, string content) =>
+        () =>
+            Try(() =>
+                {
+                    File.WriteAllText(filePath, content);
+                    return unit;
+                })
+                .ToEither(exception => Error.New(500, "error when writing file", exception));
+}
+
+public class IOOperationTests
+{
+    [Fact]
+    public void FileDoesNotExist()
+    {
+        var operation = IOOperations.ReadAllText("blah.json")();
+        operation.IsLeft.Should().BeTrue();
+        operation.IfLeft(error =>
+        {
+            error.Should().NotBeNull("error must not be null");
+            error.Code.Should().Be(500, "error code must be 500");
+            error.Exception.IsSome.Should().BeTrue("there must be an exception");
+            error.Exception.IfSome(e => e.Should().BeOfType<FileNotFoundException>($"exception must be of type {nameof(FileNotFoundException)}"));
+        });
+    }
+}
---
+++ tests/LngExt.Learnings.Tests/Usings.cs
@@ -0,0 +1,6 @@
+global using Xunit;
+
+global using FluentAssertions;
+global using LanguageExt;
+global using LanguageExt.Common;
+global using static LanguageExt.Prelude;
