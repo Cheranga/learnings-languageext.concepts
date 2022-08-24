cb7bb685d9daa169b90464d41dc3541b92bcbba6
Cheranga Hatangala <chatangala@bunnings.com.au>
2022-08-25 09:49:35 +1000

    chore: included more unit tests [ci]

--- tests/LngExt.Learnings.Tests/SideEffects/IOExtensionsTests.cs
+++ tests/LngExt.Learnings.Tests/SideEffects/IOExtensionsTests.cs
@@ -3,6 +3,33 @@
 public class IOExtensionsTests
 {
     [Fact]
+    public void ReadingAnUnExistingFile()
+    {
+        var input = "TestData/blah.json";
+
+        var operation = (from content in FileOperations.ReadAllText(input) select content).Run();
+
+        operation.IsLeft.Should().BeTrue();
+        operation.IfLeft(error =>
+        {
+            error.ToException().Should().BeOfType<FileNotFoundException>();
+            error.Code.Should().Be(500);
+            error.Message.Should().Be("error when running the IO operation");
+        });
+    }
+
+    [Fact]
+    public void ReadingAnExistingFile()
+    {
+        var input = "TestData/input.csv";
+
+        var operation = (from content in FileOperations.ReadAllText(input) select content).Run();
+
+        operation.IsRight.Should().BeTrue();
+        operation.IfRight(content => content.Should().NotBeNullOrEmpty());
+    }
+
+    [Fact]
     public void CombineReadAndWriteWithExistingFiles()
     {
         var input = "TestData/input.csv";
@@ -18,7 +45,7 @@ public class IOExtensionsTests
     }
 
     [Fact]
-    public void CombineReadAndWriteWithUnexistingFiles()
+    public void CombineReadAndWriteWithUnExistingFiles()
     {
         var input = "TestData/blah.csv";
         var output = "TestData/output.csv";
@@ -40,11 +67,12 @@ public class IOExtensionsTests
         var output = "TestData/output.csv";
 
         var operation = (
-            from a in FileOperations.ReadAllText(input)
-            from _ in FileOperations.WriteAllText(output, a)
-            select a
+            from i in FileOperations.ReadAllText(input)
+            from _ in FileOperations.WriteAllText(output, i)
+            from o in FileOperations.ReadAllText(output)
+            select o
         )
-            .Select(s => s.Length)
+            .Map(s => s.Length)
             .Run();
 
         operation.IsRight.Should().BeTrue();
