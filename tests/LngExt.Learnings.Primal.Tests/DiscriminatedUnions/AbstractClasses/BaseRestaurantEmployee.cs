89b969fad41bcd3445cdfb32672979eb190108a6
Cheranga Hatangala <cheranga@gmail.com>
2022-09-17 21:14:24 +1000

    chore: adding discriminated unions support

---
+++ tests/LngExt.Learnings.Primal.Tests/DiscriminatedUnions/AbstractClasses/BaseRestaurantEmployee.cs
@@ -0,0 +1,240 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+
+namespace LngExt.Learnings.Primal.Tests.DiscriminatedUnions.AbstractClasses;
+
+public abstract record BaseRestaurantEmployee
+{
+    private BaseRestaurantEmployee() { }
+
+    [ExcludeFromCodeCoverage(
+        Justification = "All the other domain methods, are based on this method."
+    )]
+    public TB Map<TB>(Func<Chef, TB> chefMapper, Func<Waiter, TB> waiterMapper) =>
+        this switch
+        {
+            Chef c => chefMapper(c),
+            Waiter w => waiterMapper(w),
+            _ => throw new NotSupportedException()
+        };
+
+    [ExcludeFromCodeCoverage(
+        Justification = "All the other domain methods, are based on this method."
+    )]
+    public TB Map<TB>((Func<Chef, TB> chefMapper, Func<Waiter, TB> waiterMapper) mappers) =>
+        this switch
+        {
+            Chef c => mappers.chefMapper(c),
+            Waiter w => mappers.waiterMapper(w),
+            _ => throw new NotSupportedException()
+        };
+
+    public (string, string, string) GetEmployeeInfo() =>
+        Map(c => ("Chef", c.Id, c.Name), w => ("Waiter", w.Id, w.Name));
+
+    public DayOfWeek[] GetAvailability() =>
+        this switch
+        {
+            Chef c => c.Availability,
+            Waiter w => w.Availability,
+            _ => throw new NotSupportedException()
+        };
+
+    public bool CanPrepareMeal(string cousine) =>
+        !string.IsNullOrEmpty(cousine)
+        && Map(
+            c =>
+                c.Specialities.Any(
+                    x => string.Equals(x, cousine, StringComparison.OrdinalIgnoreCase)
+                ),
+            w => false
+        );
+
+    public bool WorksIn(DayOfWeek day) =>
+        this switch
+        {
+            Chef c => c.Availability.Contains(day),
+            Waiter w => w.Availability.Contains(day),
+            _ => throw new NotSupportedException()
+        };
+
+    public record Chef(string Id, string Name, DayOfWeek[] Availability, string[] Specialities)
+        : BaseRestaurantEmployee;
+
+    public record Waiter(string Id, string Name, DayOfWeek[] Availability) : BaseRestaurantEmployee;
+}
+
+public static class DateTimeExtensions
+{
+    public static DayOfWeek[] WeekDays =>
+        new[]
+        {
+            DayOfWeek.Monday,
+            DayOfWeek.Tuesday,
+            DayOfWeek.Wednesday,
+            DayOfWeek.Thursday,
+            DayOfWeek.Friday
+        };
+
+    public static DayOfWeek[] WeekEnds => new[] { DayOfWeek.Saturday, DayOfWeek.Sunday };
+
+    public static DayOfWeek[] Week =>
+        new[]
+        {
+            DayOfWeek.Monday,
+            DayOfWeek.Tuesday,
+            DayOfWeek.Wednesday,
+            DayOfWeek.Thursday,
+            DayOfWeek.Friday,
+            DayOfWeek.Saturday,
+            DayOfWeek.Sunday
+        };
+}
+
+public static class BaseRestaurantEmployeeTests
+{
+    private static readonly Func<BaseRestaurantEmployee, (string, string, string)> getEmployeeInfo =
+        e => e.Map(c => ("Chef", c.Id, c.Name), w => ("Waiter", w.Id, w.Name));
+
+    private static readonly Func<BaseRestaurantEmployee, string, bool> canPrepareMeal = (e, s) =>
+        e.Map(c => c.Specialities.Contains(s), _ => false);
+
+    private static readonly Func<BaseRestaurantEmployee, string, bool> canPrepareMealWithTuples = (
+        e,
+        s
+    ) => e.Map((c => c.Specialities.Contains(s), _ => false));
+
+    [Fact]
+    public static void GettingEmployeeInfo()
+    {
+        BaseRestaurantEmployee chef = new BaseRestaurantEmployee.Chef(
+            "666",
+            "Cheranga",
+            DateTimeExtensions.Week,
+            new[] { "Sri Lankan" }
+        );
+        getEmployeeInfo(chef).Should().BeEquivalentTo(("Chef", "666", "Cheranga"));
+
+        chef.GetEmployeeInfo().Should().BeEquivalentTo(("Chef", "666", "Cheranga"));
+
+        BaseRestaurantEmployee waiter = new BaseRestaurantEmployee.Waiter(
+            "111",
+            "Jon Snow",
+            DateTimeExtensions.WeekEnds
+        );
+        getEmployeeInfo(waiter).Should().BeEquivalentTo(("Waiter", "111", "Jon Snow"));
+        waiter.GetEmployeeInfo().Should().BeEquivalentTo(("Waiter", "111", "Jon Snow"));
+    }
+
+    [Fact]
+    public static void GettingEmployeeInfoWithTuples()
+    {
+        BaseRestaurantEmployee chef = new BaseRestaurantEmployee.Chef(
+            "666",
+            "Cheranga",
+            DateTimeExtensions.Week,
+            new[] { "Sri Lankan" }
+        );
+        getEmployeeInfo(chef).Should().BeEquivalentTo(("Chef", "666", "Cheranga"));
+
+        BaseRestaurantEmployee waiter = new BaseRestaurantEmployee.Waiter(
+            "111",
+            "Jon Snow",
+            new[] { DayOfWeek.Friday }
+        );
+    }
+
+    [Fact]
+    public static void GettingMealPreparationSpecialities()
+    {
+        BaseRestaurantEmployee chef = new BaseRestaurantEmployee.Chef(
+            "666",
+            "Cheranga",
+            DateTimeExtensions.Week,
+            new[] { "Sri Lankan" }
+        );
+        BaseRestaurantEmployee waiter = new BaseRestaurantEmployee.Waiter(
+            "111",
+            "Jon Snow",
+            new[] { DayOfWeek.Friday }
+        );
+        canPrepareMeal(chef, "Thai").Should().BeFalse();
+        canPrepareMeal(chef, "Sri Lankan").Should().BeTrue();
+        canPrepareMeal(waiter, "any").Should().BeFalse();
+
+        chef.CanPrepareMeal("sri lankan").Should().BeTrue();
+        waiter.CanPrepareMeal("Any").Should().BeFalse();
+    }
+
+    [Fact]
+    public static void GettingMealPreparationSpecialitiesWithTuples()
+    {
+        BaseRestaurantEmployee chef = new BaseRestaurantEmployee.Chef(
+            "666",
+            "Cheranga",
+            DateTimeExtensions.Week,
+            new[] { "Sri Lankan" }
+        );
+        BaseRestaurantEmployee waiter = new BaseRestaurantEmployee.Waiter(
+            "111",
+            "Jon Snow",
+            new[] { DayOfWeek.Friday }
+        );
+        canPrepareMealWithTuples(chef, "Thai").Should().BeFalse();
+        canPrepareMealWithTuples(chef, "Sri Lankan").Should().BeTrue();
+        canPrepareMealWithTuples(waiter, "any").Should().BeFalse();
+    }
+
+    [Fact]
+    public static void GetAvailabilityTests()
+    {
+        BaseRestaurantEmployee chef = new BaseRestaurantEmployee.Chef(
+            "666",
+            "Cheranga",
+            DateTimeExtensions.Week,
+            new[] { "Western", "Thai" }
+        );
+
+        BaseRestaurantEmployee waiter = new BaseRestaurantEmployee.Waiter(
+            "888",
+            "Jon Snow",
+            DateTimeExtensions.WeekEnds
+        );
+
+        chef.GetAvailability().Should().BeEquivalentTo(DateTimeExtensions.Week);
+        waiter.GetAvailability().Should().BeEquivalentTo(DateTimeExtensions.WeekEnds);
+    }
+
+    [Fact]
+    public static void WorksInTests()
+    {
+        BaseRestaurantEmployee chef = new BaseRestaurantEmployee.Chef(
+            "666",
+            "Cheranga",
+            DateTimeExtensions.WeekDays,
+            new[] { "Western" }
+        );
+
+        BaseRestaurantEmployee waiter = new BaseRestaurantEmployee.Waiter(
+            "888",
+            "Jon Snow",
+            new[] { DayOfWeek.Tuesday }
+        );
+
+        chef.WorksIn(DayOfWeek.Friday).Should().BeTrue();
+        waiter.WorksIn(DayOfWeek.Tuesday).Should().BeTrue();
+
+        new BaseRestaurantEmployee.Chef(
+            "666",
+            "Cheranga",
+            DateTimeExtensions.WeekDays,
+            new[] {"Western"}
+        ).WorksIn(DayOfWeek.Friday).Should().BeTrue();
+
+        new BaseRestaurantEmployee.Waiter(
+            "888",
+            "Jon Snow",
+            new[] {DayOfWeek.Tuesday}
+        ).WorksIn(DayOfWeek.Tuesday).Should().BeTrue();
+    }
+}
---
+++ tests/LngExt.Learnings.Primal.Tests/DiscriminatedUnions/Interfaces/IRestaurantEmployee.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+
+namespace LngExt.Learnings.Primal.Tests.DiscriminatedUnions.Interfaces;
+
+public interface IRestaurantEmployee
+{
+    public static IRestaurantEmployee Chef(string Id, string Name, string[] Specialities) =>
+        new Chef(Id, Name, Specialities);
+
+    public static IRestaurantEmployee Waiter(string Id, string Name, DayOfWeek[] Days) =>
+        new Waiter(Id, Name, Days);
+
+    public TB Match<TB>(Func<Chef, TB> chefMapper, Func<Waiter, TB> waiterMapper) =>
+        this switch
+        {
+            Chef c => chefMapper(c),
+            Waiter w => waiterMapper(w),
+            _ => throw new NotSupportedException()
+        };
+}
+
+public record Chef(string Id, string Name, string[] Specialities) : IRestaurantEmployee;
+
+public record Waiter(string Id, string Name, DayOfWeek[] Days) : IRestaurantEmployee;
+
+public record FakeEmployee(string FakeId, string FakeName) : IRestaurantEmployee;
+
+public static class DiscriminatedTests
+{
+    [Fact]
+    public static void RestaurantStaffTests()
+    {
+        Func<IRestaurantEmployee, string> getId = e =>
+            e.Match(chef => chef.Id, waiter => waiter.Id);
+        Func<IRestaurantEmployee, DayOfWeek[]> getWorkDays = e =>
+            e.Match(
+                c => new[] { DayOfWeek.Friday, DayOfWeek.Saturday, DayOfWeek.Sunday },
+                w => w.Days
+            );
+
+        var chef = IRestaurantEmployee.Chef("CHEF-666", "Cheranga", new[] { "Pastry", "Gourmet" });
+
+        var waiter = IRestaurantEmployee.Waiter(
+            "W-001",
+            "Cheranga",
+            new[] { DayOfWeek.Friday, DayOfWeek.Saturday }
+        );
+
+        var fakeEmployee = new FakeEmployee("666", "Cheranga");
+
+        getId(chef).Should().Be("CHEF-666");
+        getId(waiter).Should().Be("W-001");
+        Assert.Throws<NotSupportedException>(() => getId(fakeEmployee));
+
+        getWorkDays(chef)
+            .Should()
+            .BeEquivalentTo(new[] { DayOfWeek.Friday, DayOfWeek.Saturday, DayOfWeek.Sunday });
+
+        getWorkDays(waiter).Should().BeEquivalentTo(new[] { DayOfWeek.Friday, DayOfWeek.Saturday });
+
+        Assert.Throws<NotSupportedException>(() => getWorkDays(fakeEmployee));
+    }
+}
